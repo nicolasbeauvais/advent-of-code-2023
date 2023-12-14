2a3fe1d318dec9f0aaa9ed60acc2284109baff08
Nicolas Beauvais <nicolas@bvs.email>
2023-12-14 09:40:24 +0100

    Day 13

--- src/2023/Day13.php
+++ src/2023/Day13.php
@@ -10,27 +10,121 @@ class Day13 extends AbstractDay
     public static function answers(): array
     {
         return [
-            [0, 0], // Test Part 1, Real Part 1
-            [0, 0], // Test Part 2, Real Part 2
+            [405, 31956], // Test Part 1, Real Part 1
+            [400, 37617], // Test Part 2, Real Part 2
         ];
     }
 
     private function parse(): array
     {
-        return file($this->input);
+        $maps = [];
+        $index = 0;
+
+        foreach (file($this->input, FILE_IGNORE_NEW_LINES) as $line) {
+            if (empty($line)) {
+                $index++;
+                continue;
+            }
+
+            if (!isset($maps[$index])) {
+                $maps[$index] = [
+                    'cols' => array_fill(0, strlen($line), ''),
+                    'rows' => []
+                ];
+            }
+
+            $maps[$index]['rows'][] = $line;
+
+            foreach (str_split($line) as $position => $char) {
+                $maps[$index]['cols'][$position] .= $char;
+            }
+        }
+
+        return $maps;
+    }
+
+
+    private function findReflectionLines(array $lines): int
+    {
+        $count = 0;
+        $linesSize = strlen($lines[0]);
+
+        for ($i = 1; $i < $linesSize; $i++) {
+            foreach ($lines as $line) {
+                $width = min($i, strlen($line) - $i);
+
+                $before = substr($line, $i - $width, $width);
+                $after = substr($line, $i, $width);
+
+                if ($before !== strrev($after)) {
+                    continue 2;
+                }
+            }
+
+            $count += $i;
+        }
+
+        return $count;
+    }
+
+    private function findReflectionLinesWithSmudge(array $lines): int
+    {
+        $count = 0;
+        $linesSize = strlen($lines[0]);
+
+        for ($i = 1; $i < $linesSize; $i++) {
+            $smudges = [];
+
+            foreach ($lines as $line) {
+                $width = min($i, strlen($line) - $i);
+
+                $before = substr($line, $i - $width, $width);
+                $after = substr($line, $i, $width);
+
+                if ($before !== strrev($after)) {
+                    $errors = array_diff_assoc(str_split($before), array_reverse(str_split($after)));
+
+                    if (count($errors) > 1 || count($smudges) > 0) {
+                        continue 2;
+                    }
+
+                    $smudges[] = $i;
+                }
+            }
+
+            if (count($smudges) === 1) {
+                $count += $smudges[0];
+            }
+        }
+
+        return $count;
     }
 
     public function part1(): int
     {
-        $this->parse();
+        $maps = $this->parse();
 
-        return self::TODO;
+        $total = 0;
+
+        foreach ($maps as $map) {
+            $total += $this->findReflectionLines($map['rows']);
+            $total += 100 * $this->findReflectionLines($map['cols']);
+        }
+
+        return $total;
     }
 
     public function part2(): int
     {
-        $this->parse();
+        $maps = $this->parse();
 
-        return self::TODO;
+        $total = 0;
+
+        foreach ($maps as $map) {
+            $total += $this->findReflectionLinesWithSmudge($map['rows']);
+            $total += 100 * $this->findReflectionLinesWithSmudge($map['cols']);
+        }
+
+        return $total;
     }
 }
