008afc2e2fe0d7086bdbfa66093889ed6b1d1a2d
Nicolas Beauvais <nicolas@bvs.email>
2023-12-16 11:57:39 +0100

    Day 15

--- src/2023/Day15.php
+++ src/2023/Day15.php
@@ -10,27 +10,57 @@ class Day15 extends AbstractDay
     public static function answers(): array
     {
         return [
-            [0, 0], // Test Part 1, Real Part 1
-            [0, 0], // Test Part 2, Real Part 2
+            [1320, 520500], // Test Part 1, Real Part 1
+            [145, 213097], // Test Part 2, Real Part 2
         ];
     }
 
     private function parse(): array
     {
-        return file($this->input);
+        return explode(',', trim(file_get_contents($this->input)));
+    }
+
+    private function hash(string $string): int
+    {
+        return array_reduce(
+            str_split($string),
+            fn ($hash, $char) => ($hash + ord($char)) * 17 % 256,
+            0
+        );
     }
 
     public function part1(): int
     {
-        $this->parse();
-
-        return self::TODO;
+        return array_reduce($this->parse(), fn ($total, $item) => $total + $this->hash($item), 0);
     }
 
     public function part2(): int
     {
-        $this->parse();
+        $boxes = array_fill(0, 255, []);
 
-        return self::TODO;
+        foreach ($this->parse() as $item) {
+            if ($item[-1] === '-') {
+                $label = substr($item, 0, -1);
+
+                unset($boxes[$this->hash($label)][$label]);
+            } else {
+                [$label, $focalLength] = explode('=', $item);
+
+                $boxes[$this->hash($label)][$label] = $focalLength;
+            }
+        }
+
+        $total = 0;
+
+        foreach ($boxes as $index => $box) {
+            $position = 1;
+
+            foreach ($box as $focalLength) {
+                $total += ($index + 1) * $position * $focalLength;
+                $position++;
+            }
+        }
+
+        return $total;
     }
 }
